3c3136779a5d1d17464efa1e238195fce0fbd61b
Jorge L. Gaston Gonzalez <gaston@novateksoluciones.com>
2023-09-05 18:44:05 -0300

    Haciendo cambios en Github Jorge.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/persona', personaRoute);
 
 app.get('/', (req, res) => {
-  res.send("Hola desde UTEC Taller DevOps");
+  res.send("Hola desde UTEC Taller DevOps 2023");
 });
 
 app.listen(port, () => {
